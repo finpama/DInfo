4be931eea7b1aba5879ed3175f5e3df863992624
Finpama <phma020@gmail.com>
2026-01-15 09:54:35 -0300

    Remoção bboxes e melhoria patterns

--- scrap.py
+++ scrap.py
@@ -4,52 +4,51 @@ import re
 def gerarLinha(di_path):
     
     linha = {}
-
-    coordinates = {
-        'Processo': (102.2, 91.1, 151.2, 102.2),
-        'DI': (85.4, 12.7, 151.0, 26.9),
-        
-        'II': (388, 435.4, 470.7, 450.7),
-        'IPI': (388, 448.3, 470, 465.4),
-        'PIS': (388, 461, 470, 480),
-        'COFINS': (388, 475, 470, 495),
-        'Antidumping': (388, 490, 470, 505),
-    }
     
     patterns = {
-        'VMLE': r"VMLE[^\d]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*)",
-        'Frete': r"Frete[^\d]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*)",
-        'Seguro': r"Seguro[^\d]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*)",
-        'CIF': r"VMLE\+FRETE\+SEGURO[^\d]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*)",
-        'Acréscimos': r"Acréscimos[^\d]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*)",
+        #Coluna: (Página -1, Padrão)
+        'Processo': r"REF\. CLIENTE[^\d]*? (.*\d[a-z])",
+        'DI': r"Declaração[^\d\n]*(.*?-\d)",
         
-        'Capatazia': r"Capatazia[^\d]*(\d.*)",
-        'Anuência LI': r"Taxa anuencia [^\d]*(\d.*)",
-        'AFRMM': r"AFRMM[^\d]*(\d.*)",
-        'Siscomex': r"TX siscomex[^\d]*(\d.*)",
+        'II': r"I\.I\..*?,\d{2} (.*)",
+        'IPI': r"I\.P\.I\..*?,\d{2} (.*\d)",
+        'PIS': r"Pis/Pasep.*?,\d{2} (.*\d)",
+        'COFINS': r"Cofins.*?,\d{2} (.*\d)",
+        
+        'VMLE': r"VMLE[^\d\n]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*\d)",
+        'Frete': r"Frete[^\d\n]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*\d)",
+        'Seguro': r"Seguro[^\d\n]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*\d)",
+        'CIF': r"VMLE\+FRETE\+SEGURO[^\d\n]*(\d.*?,\d{2}) / (\d.*?,\d{2}) / (\d.*\d)",
+        
+        'Capatazia': r"Capatazia - [^\d\n]*(\d.*\d)",
+        'AFRMM': r"AFRMM - [^\d\n]*(\d.*\d)",
+        'Siscomex': r"TX siscomex - [^\d\n]*(\d.*\d)",
     }
     
     with pdfplumber.open(di_path) as di:
-        info = di.pages[1].extract_text()
-
-        for field, bbox in coordinates.items():
-            if coordinates[field] != None:
-                
-                pageNum = 1 if field == 'Processo' else 0 # Com excessão do processo, todos os campos estão na primeira página
-
-                bboxInfo = di.pages[pageNum].within_bbox(bbox).extract_text()
-                linha[field] = bboxInfo
-                
+        
         for field, pattern in patterns.items():
             
+            match field:
+                case 'VMLE' | 'Frete' | 'Seguro' | 'CIF':
+                    groupIndex = 2
+                case _:
+                    groupIndex = 1
+                
+            info = ''
+            
+            for page in di.pages:
+                info += page.extract_text() + '\n\n\n\n'
+            
             match = re.search(pattern, info, re.IGNORECASE)
-
-            if field == 'Capatazia' or field == 'Anuência LI' or field == 'AFRMM' or field == 'Siscomex':
-                if match != None:
-                    linha[field] = match.group(1)
-                else:
-                    linha[field] = 'Não encontrado'
+            
+            if field == 'Processo' and match == None:
+                match = re.search(r"REF\. CLIENTE[^\d]*? (.*\d)", info, re.IGNORECASE)
+                
+            
+            if match != None:
+                linha[field] = match.group(groupIndex)
             else:
-                linha[field] = match.group(2)
-    
+                linha[field] = 'Não encontrado'
+        
     return linha
